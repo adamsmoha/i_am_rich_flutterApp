2435e7b1149c96970b1e485895274d3560ef207c
adamsmoha <adamsmoha@2019.com>
2023-11-12 03:16:37 +0000

    Initial message

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.asset('images/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
